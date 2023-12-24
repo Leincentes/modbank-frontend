57de770829a56091096001fd47a3e20130faeacf
Niel <leincentes@gmail.com>
2023-12-24 13:48:05 +0800

    adding files

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#BC9C66",
+        secondary: "#fff",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgba(209, 172, 149, 0.1)",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
